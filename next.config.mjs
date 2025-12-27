115ebaf24994823e94a6369e90a0b6c5046c4131
Rosie Kwon <rosiekwon514@gmail.com>
2025-12-27 04:28:22 -0800

    chore: disable eslint blocking during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   experimental: {
     serverComponentsExternalPackages: ["@supabase/supabase-js", "@supabase/ssr"],
   },
